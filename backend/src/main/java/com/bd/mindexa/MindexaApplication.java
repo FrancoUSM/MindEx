86e4b03433046fefe416ccb2b851041aa2c01ab3
Franco <franco.hernandez@mindexa.cl>
2026-06-09 06:08:57 -0400

    mindexaapplication

--- backend/src/main/java/com/bd/mindexa/MindexaApplication.java
+++ backend/src/main/java/com/bd/mindexa/MindexaApplication.java
@@ -1,13 +1,26 @@
+
 package com.bd.mindexa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MindexaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MindexaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MindexaApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "Mindex backend funcionando en Azure";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
 }
