b71dc19ef985a5cea952d1e1f06f5d46bd1b3674
Franco <franco.hernandez@mindexa.cl>
2026-07-28 12:08:40 -0400

    arreglos de tipado en models

--- backend/src/main/java/com/bd/mindexa/repository/suscripcion/RepositorioSuscripcion.java
+++ backend/src/main/java/com/bd/mindexa/repository/suscripcion/RepositorioSuscripcion.java
@@ -3,10 +3,14 @@ package com.bd.mindexa.repository.suscripcion;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bd.mindexa.models.suscripcion.Suscripcion;
 
 public interface RepositorioSuscripcion extends JpaRepository<Suscripcion,Integer>{
 Optional<Suscripcion> findById(int id_suscripcion);
-Optional<Suscripcion> findByIdEmpresa(int idEmpresa); 
+@Query("SELECT s FROM Suscripcion s WHERE s.id_empresa = :idEmpresa") // Cambia 'empresaId' por el nombre real de tu propiedad
+Optional<Suscripcion> findByIdEmpresa(@Param("idEmpresa") int idEmpresa);
+
 }
