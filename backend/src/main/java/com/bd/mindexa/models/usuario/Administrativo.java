94b057e2e8e126cf492d61c635faa3295e6ff206
Franco <franco.hernandez@mindexa.cl>
2026-07-28 01:31:59 -0400

    arreglos en administrador page

--- backend/src/main/java/com/bd/mindexa/models/usuario/Administrativo.java
+++ backend/src/main/java/com/bd/mindexa/models/usuario/Administrativo.java
@@ -10,10 +10,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-@Getter
-@Setter
+
+
 @Data
 @Entity
 @Table(name = "administrativo")
@@ -25,7 +23,7 @@ public class Administrativo {
     private int id_administrativo;
     
     @ManyToOne
-@JoinColumn(name = "id_usuario")
+    @JoinColumn(name = "id_usuario")
     private Usuario usuario;
     
     private LocalDateTime creado_en;
